849870240ea3b34df9f93d2911183da2033ce323
Acquity2 <54288089+Acquity2@users.noreply.github.com>
2021-02-12 01:00:25 +0800

    Add files via upload

--- scripts/readFlash.lua
+++ scripts/readFlash.lua
@@ -1,5 +1,5 @@
-local component = require(component)
-local sides = require(sides)
+local component = require('component')
+local sides = require('sides')
 
 readFlash = {}
 
@@ -14,5 +14,20 @@ function readFlash.getInputFluids(side,n)
 	local _inputFluids = _table.inputFluids
 	return _inputFluids
 end
+
+function readFlash.count(side)
+	local _size = component.inventory_controller.getInventorySize(side)
+	_size = _size - 1
+	local _table = component.inventory_controller.getAllStacks(side)
+	local a = _table.getAll()
+	local N = 0
+	for i = 0,_size,1 do
+		if a[i].label ~= nil then N = N + 1 end
+	end
+	return N
+end
+
+	
+
 
 return readFlash
