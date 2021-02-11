cf0f7ece98747dfdb24e5c996ee1e8b2d746edb7
Acquity2 <54288089+Acquity2@users.noreply.github.com>
2021-02-12 01:23:47 +0800

    Add files via upload

--- scripts/compare.lua
+++ scripts/compare.lua
@@ -17,37 +17,35 @@ local list1,list2 = {} --list1为读取闪存中list，list2为输入箱子中�
 function compare.listCompare(list1,list2)
 	local _leng1 = compare.table_leng(list1)
 	local _leng2 = compare.table_leng(list2)
+	local _result = false
 	if _leng1 ~= _leng2 then 
+		_result = false
 		goto BRE
 	end
 	local _table = {}
 	for i = 1,_leng1,1 do
-		local _Bool = list1[i][1] == list2[i][1] and list1[i][2] == list2[i][2]
-		_Bool = not _Bool
-		if _Bool then
-			goto BRE
-		end
+		_table[i] = list1[i][1] == list2[i][1] and list1[i][2] == list2[i][2]
 	end
-	return true
-	goto BRE1
+	_result = true
+	for i = 1,_leng1,1 do
+		_result = _result and _table[i]
+	end
 	::BRE::
-	return false
-	::BRE1::
+	return _result
 end
 
 function compare.main()
 	local _num = readFlash.count(sides.down)
 	local _itemList = getInputItemList.main(sides.up)
 	local _inputList = {}
+	local _result = nil
 	for i = 1,_num,1 do
 		local _inputList = readFlash.getInputItems(sides.down,i)
 		if compare.listCompare(_inputList,_itemList) then
-			return i
-			goto BRE2
+			_result = i
 		end
 	end
-	return nil
-	::BRE2::
+	return _result
 end
 
 
