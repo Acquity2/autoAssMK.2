452e5067f127738ab8220a74d2e94faf7204cad3
Acquity2 <54288089+Acquity2@users.noreply.github.com>
2021-02-15 16:18:55 +0800

    Add files via upload

--- scripts/chipDatabase.lua
+++ scripts/chipDatabase.lua
@@ -5,11 +5,22 @@ chipDatabase['量子处理器'] = 'ev'
 chipDatabase['纳米处理器集群'] = 'ev'
 chipDatabase['数据磁共振电路'] = 'ev'
 
+chipDatabase['gt.metaitem.01.32704.name'] = 'ev'
+chipDatabase['gt.metaitem.03.32085.name'] = 'ev'
+chipDatabase['gt.metaitem.03.32083.name'] = 'ev'
+chipDatabase['bwMetaGeneratedItem0.8.name'] = 'ev'
+
 chipDatabase['处理器主机'] = 'iv'
 chipDatabase['精英纳米电脑'] = 'iv'
 chipDatabase['量子处理器集群'] = 'iv'
 chipDatabase['晶体处理器'] = 'iv'
 chipDatabase['精英磁共振电路'] = 'iv'
+
+chipDatabase['gt.metaitem.01.32705.name'] = 'iv'
+chipDatabase['gt.metaitem.03.32084.name'] = 'iv'
+chipDatabase['gt.metaitem.03.32086.name'] = 'iv'
+chipDatabase['gt.metaitem.03.32089.name'] = 'iv'
+chipDatabase['bwMetaGeneratedItem0.9.name'] = 'iv'
 
 chipDatabase['纳米处理器主机'] = 'luv'
 chipDatabase['大型量子电脑'] = 'luv'
@@ -17,23 +28,48 @@ chipDatabase['湿件处理器'] = 'luv'
 chipDatabase['晶体处理器集群'] = 'luv'
 chipDatabase['大师磁共振电路'] = 'luv'
 
+chipDatabase['gt.metaitem.01.32706.name'] = 'luv'
+chipDatabase['gt.metaitem.03.32087.name'] = 'luv'
+chipDatabase['gt.metaitem.03.32092.name'] = 'luv'
+chipDatabase['gt.metaitem.03.32096.name'] = 'luv'
+chipDatabase['bwMetaGeneratedItem0.10.name'] = 'luv'
+
 chipDatabase['量子处理器主机'] = 'zpm'
 chipDatabase['终极晶体电脑'] = 'zpm'
 chipDatabase['湿件处理器集群'] = 'zpm'
 chipDatabase['生物处理器'] = 'zpm'
 chipDatabase['顶级磁共振电路'] = 'zpm'
 
+chipDatabase['gt.metaitem.03.32088.name'] = 'zpm'
+chipDatabase['gt.metaitem.03.32090.name'] = 'zpm'
+chipDatabase['gt.metaitem.03.32093.name'] = 'zpm'
+chipDatabase['gt.metaitem.03.32097.name'] = 'zpm'
+chipDatabase['bwMetaGeneratedItem0.11.name'] = 'zpm'
+
 chipDatabase['晶体处理器主机'] = 'uv'
 chipDatabase['湿件处理器超级电脑'] = 'uv'
 chipDatabase['生物处理器集群'] = 'uv'
 chipDatabase['超导磁共振电路'] = 'uv'
 
+chipDatabase['gt.metaitem.03.32091.name'] = 'uv'
+chipDatabase['gt.metaitem.03.32094.name'] = 'uv'
+chipDatabase['gt.metaitem.03.32098.name'] = 'uv'
+chipDatabase['bwMetaGeneratedItem0.12.name'] = 'uv'
+
 chipDatabase['湿件主机'] = 'uhv'
 chipDatabase['生物处理器超级电脑'] = 'uhv'
 chipDatabase['终极磁共振电路'] = 'uhv'
 
+chipDatabase['gt.metaitem.03.32095.name'] = 'uhv'
+chipDatabase['gt.metaitem.03.32099.name'] = 'uhv'
+chipDatabase['bwMetaGeneratedItem0.13.name'] = 'uhv'
+
 chipDatabase['生物主机'] = 'uev'
 chipDatabase['生物磁共振电路'] = 'uev'
+
+chipDatabase['gt.metaitem.03.32120.name'] = 'uev'
+chipDatabase['bwMetaGeneratedItem0.14.name'] = 'uev'
+
 
 return chipDatabase
 
