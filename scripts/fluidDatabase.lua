29bfac86249f2b704709c11e362d56466249756a
Acquity2 <54288089+Acquity2@users.noreply.github.com>
2021-02-15 15:00:06 +0800

    Add files via upload

--- scripts/fluidDatabase.lua
+++ scripts/fluidDatabase.lua
@@ -1,8 +1,8 @@
 fluidDatabase = {}
 fluidDatabase['熔融量子单元'] = '熔融量子锭'
 fluidDatabase['熔融Naquadria单元'] = '熔融超能硅岩'
-fluidDatabase['gt.metaitem.99.314.name'] = 'fluid.molten.solderingallory'
-fluidDatabase['gt.metaitem.01.30724.name'] = 'fluid.lubrican'
-fluidDatabase['gt.metaitem.99.635.name'] = 'fluid.molten.styrenebutadienerubber'
-fluidDatabase['Coolant Cell'] = 'IC2 Coolant'
+fluidDatabase['fluid.molten.solderingallory'] = 'gt.metaitem.99.314.name'
+fluidDatabase['fluid.lubrican'] = 'gt.metaitem.01.30724.name'
+fluidDatabase['fluid.molten.styrenebutadienerubber'] = 'gt.metaitem.99.635.name'
+fluidDatabase['IC2 Coolant'] = 'Coolant Cell'
 return fluidDatabase
