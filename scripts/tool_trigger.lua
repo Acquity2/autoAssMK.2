ddb52701c7f12752a6755c3d2689b8cfaaee72d4
Acquity2 <54288089+Acquity2@users.noreply.github.com>
2021-02-15 21:14:58 +0800

    Add files via upload

--- scripts/tool_trigger.lua
+++ scripts/tool_trigger.lua
@@ -3,19 +3,23 @@ local os = require('os')
 local component = require('component')
 local sides = require('sides')
 local event = require('event')
+local _round = 1
 
 function trigger()
+	os.execute('cls')
+	print('Start round '.._round)
 	local _table1 = component.me_controller.getItemsInNetwork()
 	local _table2 = getInputItemList.main(sides.up)
 	if _table2[1] ~= nil then
 		if _table1.n ~= 0 then
 			print('Item in InputBus,Terminated')
+			os.sleep(0.5)
 		else
 			main.start()
 		end
 	end
+	_round = _round + 1
 end
 
 os.execute('cls')
-print('Start')
-event.timer(1.5,trigger,math.huge)
+event.timer(2,trigger,math.huge)
