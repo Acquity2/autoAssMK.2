b439ac89383059066a99f4e3526c17747ae6f7a2
Acquity2 <54288089+Acquity2@users.noreply.github.com>
2021-02-15 15:39:53 +0800

    Add files via upload

--- scripts/tool_trigger.lua
+++ scripts/tool_trigger.lua
@@ -2,8 +2,9 @@ local main = require('main')
 local os = require('os')
 local component = require('component')
 local sides = require('sides')
+local event = require('event')
 
-while true do
+function main()
 	local _table1 = component.me_controller.getItemsInNetwork()
 	local _table2 = getInputItemList.main(sides.up)
 	if _table2[1] ~= nil then
@@ -15,3 +16,7 @@ while true do
 	end
 	os.sleep(0.5)
 end
+
+os.execute('cls')
+print('Start')
+event.timer(1,main,math.huge)
